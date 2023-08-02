e858b11d9310a407325bf0b8c71f207fc463bcec
keen <kelim1221@gmail.com>
2023-08-02 12:52:57 +0800

    completed backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+//Import mealGenerator function
+const {mealGenerator} = require('./routes/mealGenerator');
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let array = mealGenerator();
+  res.send(array);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+
+// Server Setup - Seeting REST API Server to Port 8080
+app.listen(3000,'0.0.0.0', () => {
+  console.log('Server started at PORT 3000');
+})
